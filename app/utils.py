aac459c4180175c2a375ad8a75239f1b450cb4a6
Arseniy <Arsgusev2020@gmail.com>
2025-09-16 00:17:05 +0300

    Добавил этот файл для хэширования паролей и создания JWT-токенов

---
+++ app/utils.py
@@ -0,0 +1,22 @@
+import os
+from passlib.context import CryptContext
+from authx import AuthX, AuthXConfig
+from dotenv import load_dotenv
+
+load_dotenv()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")  # берём из .env
+config.JWT_ACCESS_COOKIE_NAME = os.getenv("JWT_ACCESS_COOKIE_NAME", "my_access_token")
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=config)
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed: str):
+    return pwd_context.verify(password, hashed)
