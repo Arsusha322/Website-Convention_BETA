870a78700f7f118aa6496798da9ffcfd44f56b66
Arseniy <Arsgusev2020@gmail.com>
2025-09-16 00:07:32 +0300

    Добавил lifespan и переместил все эндпоинты в отдельные файлы

--- app/main.py
+++ app/main.py
@@ -1,44 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from passlib.context import CryptContext
-import os
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.database import Base, engine
+from app.routs import users
 
-from app.database import get_db, Base, engine
-from app.models import UserData, UserHistory
-from app.schemas import UserCreate, UserResponse
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI(lifespan=lifespan)
 
-app = FastAPI(title="Audio-to-Text Converter API")
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-def verify_password(password: str, hashed: str):
-    return pwd_context.verify(password, hashed)
-
-
-@app.post("/users", response_model=UserResponse)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(UserData).filter(UserData.email == user.email).first()
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email уже зарегистрирован")
-
-    new_user = UserData(
-        email=user.email,
-        password_hash=hash_password(user.password)
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-@app.get("/users/{user_id}", response_model=UserResponse)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(UserData).filter(UserData.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="Пользователь не найден")
-    return user
+app.include_router(users.router, prefix="/users", tags=["users"])
