5bf021c910c26cdd39cf9df5a9f814dfd601aa88
Arseniy <Arsgusev2020@gmail.com>
2025-09-16 11:46:00 +0300

    Добавил схемы для работы с историей конвертаций

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator
+from datetime import datetime
 import re
 
 
@@ -42,3 +43,17 @@ class UserLogin(UserBase):
 class UserLoginResponse(BaseModel):
     access_token: str
     token_type: str
+
+
+class UserHistoryCreate(BaseModel):
+    file_name: str
+    text_result: str
+
+class UserHistoryResponse(BaseModel):
+    id: int
+    file_name: str
+    text_result: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
