3ac577f13b450c3608f12b2af178bcae6028c40d
Arseniy <Arsgusev2020@gmail.com>
2025-09-16 11:45:25 +0300

    начал делать историю конвертаций, но появилась проблема "Missing CSRF token"

--- app/routs/users.py
+++ app/routs/users.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import UserData
-from app.schemas import UserCreate, UserResponse, UserLogin, UserLoginResponse
-from app.utils import hash_password, verify_password, security
+from app.models import UserData, UserHistory
+from app.schemas import UserCreate, UserResponse, UserLogin, UserLoginResponse, UserHistoryCreate, UserHistoryResponse
+from app.utils import hash_password, verify_password, security, config
 
 router = APIRouter()
 
@@ -22,7 +22,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.post("/login", response_model=UserLoginResponse)
-def login(user: UserLogin, db: Session = Depends(get_db)):
+def login(user: UserLogin, response: Response, db: Session = Depends(get_db)):
     try:
         db_user = db.query(UserData).filter(UserData.email == user.email).first()
         if not db_user:
@@ -32,7 +32,21 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
             raise HTTPException(status_code=401, detail="Неверный email или пароль")
 
         access_token = security.create_access_token(uid=str(db_user.id))
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, access_token)
         return UserLoginResponse(access_token=access_token, token_type="bearer")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/history", response_model=UserHistoryResponse)
+def add_history(data: UserHistoryCreate, db: Session = Depends(get_db), user_id: str = Depends(security.get_current_subject)):
+    new_record = UserHistory(user_id = user_id, file_name = data.file_name, text_result = data.text_result)
+    db.add(new_record)
+    db.commit()
+    db.refresh(new_record)
+    return new_record
+
+@router.get("/history", response_model=list[UserHistoryResponse])
+def get_history(db: Session = Depends(get_db), user_id: str = Depends(security.get_current_subject)):
+    return db.query(UserHistory).filter(UserHistory.user_id == user_id).order_by(UserHistory.created_at.desc()).all()
