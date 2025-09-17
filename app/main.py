17ab11b57e0530af54d4ee52640c37b3c270ce06
Arseniy <Arsgusev2020@gmail.com>
2025-09-17 22:04:22 +0300

    Подключил filse к всему

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.database import Base, engine
-from app.routs import users
+from app.routs import users, files
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,3 +11,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(files.router, prefix="/files", tags=["files"])
