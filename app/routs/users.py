00fbfc80b81b396c1ccc80600427494787da1ad9
Arseniy <Arsgusev2020@gmail.com>
2025-09-19 18:02:41 +0300

    Добавил асинхронность

--- app/routs/users.py
+++ app/routs/users.py
@@ -8,7 +8,7 @@ from app.utils import hash_password, verify_password, create_access_token, get_c
 router = APIRouter()
 
 @router.post("/", response_model=UserResponse)
-def create_user(user: UserCreate, response: Response, db: Session = Depends(get_db)):
+async def create_user(user: UserCreate, response: Response, db: Session = Depends(get_db)):
     if db.query(UserData).filter(UserData.email == user.email).first():
         raise HTTPException(status_code=400, detail="Email уже зарегистрирован")
 
@@ -22,7 +22,7 @@ def create_user(user: UserCreate, response: Response, db: Session = Depends(get_
 
 
 @router.post("/login", response_model=UserLoginResponse)
-def login(user: UserLogin, response: Response, db: Session = Depends(get_db)):
+async def login(user: UserLogin, response: Response, db: Session = Depends(get_db)):
     db_user = db.query(UserData).filter(UserData.email == user.email).first()
     if not db_user or not verify_password(user.password, db_user.password_hash):
         raise HTTPException(status_code=401, detail="Неверный email или пароль")
@@ -32,12 +32,12 @@ def login(user: UserLogin, response: Response, db: Session = Depends(get_db)):
     return UserLoginResponse(access_token=access_token, token_type="bearer")
 
 @router.post("/logout")
-def logout(response: Response):
+async def logout(response: Response):
     response.delete_cookie("access_token")
     return {"message": "Вы успешно вышли"}
 
 
 @router.get("/history", response_model=list[UserHistoryResponse])
-def get_history(db: Session = Depends(get_db), access_token: str | None = Cookie(default=None)):
+async def get_history(db: Session = Depends(get_db), access_token: str | None = Cookie(default=None)):
     user_id = get_current_user(access_token)
     return db.query(UserHistory).filter(UserHistory.user_id == user_id).order_by(UserHistory.created_at.desc()).all()
