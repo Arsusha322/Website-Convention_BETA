d754da7fc4595ef4c5c1f5e6edc5f976b2caca05
Arseniy <Arsgusev2020@gmail.com>
2025-10-04 20:49:53 +0300

    добавил CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.database import Base, engine
 from app.routers import users, files
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,6 +11,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:63342"],  # твой фронт
+    allow_credentials=True,
+    allow_methods=["*"],  # разрешаем все методы (POST, GET, OPTIONS и т.д.)
+    allow_headers=["*"],  # разрешаем все заголовки
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(files.router, prefix="/files", tags=["files"])
