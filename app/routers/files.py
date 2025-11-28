6a4f0306e4456c07031012585ebfc4e46d46cfd1
Arseniy <Arsgusev2020@gmail.com>
2025-11-28 18:40:17 +0300

    сделал загрузку файлов( загружает файлы локально и не конвертирует)

--- app/routers/files.py
+++ app/routers/files.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Cookie, UploadFile, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
+from app.models import UserHistory
 from app.schemas import UserHistoryResponse
 from app.utils.password_and_token import get_current_user
 import aiofiles
@@ -9,25 +10,24 @@ router = APIRouter()
 
 
 @router.post("/uploadfile/", response_model=UserHistoryResponse)
-async def upload_file(file: UploadFile, access_token: str | None = Cookie(default=None), db: Session = Depends(get_db)):
+async def upload_file(file: UploadFile, access_token: str | None = Cookie(default=None), db: AsyncSession = Depends(get_db)):
     user_id = get_current_user(access_token)
+
     if not file.filename.endswith((".mp3", ".wav")):
         raise HTTPException(status_code=400, detail="Неподдерживаемый формат файла")
 
     file_path = f"uploaded_files/{file.filename}"
+
     async with aiofiles.open(file_path, "wb") as f:
-        context = await file.read()
-        await f.write(context)
+        data = await file.read()
+        await f.write(data)
 
-    """     ждем антона
+    converted_text = "А у носатого хоббита, мозг опух"
 
-    convertated_text = тут будет функция конвертации взятая из utils(которую я и жду)
-
-    new_record = UserHistory(user_id=user_id, file_name=file.filename, text_result=convertated_text)
+    new_record = UserHistory(user_id=user_id, file_name=file.filename, text_result=converted_text)
 
     db.add(new_record)
-    db.commit()
-    db.refresh(new_record)
+    await db.commit()
+    await db.refresh(new_record)
 
     return new_record
-    """
